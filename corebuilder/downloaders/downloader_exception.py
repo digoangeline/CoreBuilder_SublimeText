9a19e801454d0a5d498c4fee40cdcb049d7f587f
Rodrigo Angeline <digoangeline@gmail.com>
2013-12-19 20:08:19 -0200

    Bugfix

--- corebuilder/downloaders/downloader_exception.py
+++ corebuilder/downloaders/downloader_exception.py
@@ -6,6 +6,6 @@ class DownloaderException(Exception):
     """If a downloader could not download a URL"""
 
     def __str__(self):
-    	auth = DownloadAuthenticator()
-    	auth.clear_user_auth()
+        auth = DownloadAuthenticator()
+        auth.clear_user_auth()
         return self.args[0]
